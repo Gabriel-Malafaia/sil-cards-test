e46db3d8b600cfbe4176a1c64c59c079dac8941b
gabriel-mlf <gabrielbsnsp@gmail.com>
2023-10-02 09:09:44 -0300

    feat: deploy database

--- src/service/api/index.ts
+++ src/service/api/index.ts
@@ -1,8 +1,12 @@
 import axios from "axios";
 
+// Use to deployed database
+export const deployURL = "https://sil-cards-test-database.vercel.app/";
+
+// Use to local requests
 export const baseURL = "http://localhost:3000";
 
 export const api = axios.create({
-  baseURL,
+  baseURL: deployURL,
   timeout: 5000,
 });
